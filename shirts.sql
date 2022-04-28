70332fe726ac6b9285344dc5b5e5697a5a3846d1
gcbostic56 <101133682+gcbostic56@users.noreply.github.com>
2022-04-28 09:39:45 -0400

    Add files via upload

---
+++ shirts.sql
@@ -0,0 +1,43 @@
+/*create database shirts_db*/
+/*use shirts_db*/
+/*create table shirts*/
+
+
+/*Q1 insert into shirts (shirt_id, article, color, shirt_size, last_worn)
+values 
+(1, 't-shirt', 'white', 'S', 10)
+(2, 't-shirt', 'green', 'S', 200), 
+(3, 'polo shirt', 'black', 'M', 10), 
+(4, 'tank top', 'blue', 'S', 50), 
+(5, 't-shirt', 'pink', 'S', 0), 
+(6, 'polo shirt', 'red', 'M', 5), 
+(7, 'tank top', 'white', 'S', 200), 
+(8, 'tank top', 'blue', 'M', 15);
+select * from shirts */
+/*Q2 insert into shirts (shirt_id, article, color, shirt_size, last_worn)
+values ('9', 'polo shirt', 'purple', 'M', '50')*/
+/*Q3 select article, color from shirts*/
+/*Q4 select article, color, shirt_size, last_worn
+from shirts
+where shirt_size = 'M'*/
+/* Q5 set sql_safe_updates = 0;
+update shirts
+set shirt_size = 'L'
+where article = 'polo shirt'*/
+/*Q6 update shirts
+set last_worn = '0'
+where last_worn = '15'*/
+/* Q7 update shirts
+set shirt_size = 'XS', color = 'off white'
+where color = 'white' */
+/*Q8 delete from shirts
+where last_worn = '200'*/
+/* Q9 delete from shirts
+where article = 'tank top' */
+/* Q10 delete from shirts */
+/* Q11 drop table shirts */
+
+
+
+
+
